ad7590ce70c10df4fea7f51e3c1794e03d95f797
Scott Yeager <yeagersm@gmail.com>
2023-12-06 16:43:25 -0800

    Change to compare tags as build condition

--- build-push-images.sh
+++ build-push-images.sh
@@ -7,16 +7,55 @@
 # We'll pass in the TF Hub token as a repo secret through the workflow
 tfhub_token=$1
 
+# Take a Docker image name and convert it to flist on the TF Hub
+tfhub_push () {
+  curl -X POST -F image=$1 -H "Authorization: bearer $tfhub_token" https://hub.grid.tf/api/flist/me/docker
+}
+
+# Check if any files, excluding READMEs, under a path changed in the last commit
+diffed () {
+  git diff --name-only HEAD^ HEAD $1 | grep -v README > /dev/null;
+}
+
+# Check if any files under a path, excluding READMEs, have changes between
+# the last two tags. Intended for a workflow that only runs on tag pushes
+diffed_tags () {
+  last_tag=$(git describe --tags --abbrev=0)
+  previous_tag=$(git describe --tags --abbrev=0 $last_tag^)
+  git diff --name-only $last_tag $previous_tag $1 | grep -v README > /dev/null;
+}
+
 # Pulls in variables: deb_parents and zinit_version
 source config.sh
 
-for parent in ${deb_parents[@]}; do 
-  name=ghcr.io/scottyeager/$parent
+# First build our apt/deb based distros. The Docker images will also be used as
+# bases for many images below, so order matters
+if diffed_tags deb-base; then
+  for parent in ${deb_parents[@]}; do 
+    name=ghcr.io/scottyeager/$parent
 
-  docker buildx build ./deb-base \
-                      --build-arg PARENT=$parent \
-                      --build-arg ZINIT_VERSION=$zinit_version \
+    docker buildx build ./deb-base \
+                        --build-arg PARENT=$parent \
+                        --build-arg ZINIT_VERSION=$zinit_version \
+                        --tag $name
+    docker push $name
+    tfhub_push $name
+  done
+fi
+
+# Alpine can also be used as a base image, so build it separately too
+# TODO
+
+# Now build all the rest of the images
+image_dirs=$(ls -d */ | grep -v 'deb-base\|alpine')
+
+for path in $image_dirs; do
+  if diffed_tags $path; then
+    name=ghcr.io/scottyeager/${path///} # Strip trailing / from paths
+
+    docker buildx build $path \
                       --tag $name
-  docker push $name
-  curl -X POST -F image=$name -H "Authorization: bearer $tfhub_token" https://hub.grid.tf/api/flist/me/docker
+    docker push $name
+    tfhub_push $name
+  fi
 done
