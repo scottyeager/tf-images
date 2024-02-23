31819322ca08860f43cf3528bfe2b547e5ff0485
Scott Yeager <yeagersm@gmail.com>
2024-02-23 12:22:08 -0800

    Single Dockerfile method

--- fullvm/build.sh
+++ fullvm/build.sh
@@ -3,23 +3,11 @@
 path=$1
 name=$(basename $path)
 
-# Build the image from Dockerfile
-pushd $path
-docker buildx build -t $name .
+cd $path
 
-# Extract image into tar archive
-container=$(docker create $name bash)
-docker export $container --output=${name}.tar
-docker rm $container
+# Generates the tar archive
+docker buildx build --output=. .
+mv img.tar.gz ${name}.tar.gz
 
-# Run touchup script, using Docker to avoid `sudo`
-container=$(docker create -e ARCHIVE=${name}.tar --entrypoint /touchup.sh ubuntu)
-docker cp ${name}.tar $container:/
-docker cp ../touchup.sh $container:/
-
-docker start -a $container
-docker cp $container:/${name}.tar.gz ./
-
-docker rm $container
-rm ${name}.tar
-popd
+# Also tag and save the image
+docker buildx build --target=image -t $name .
