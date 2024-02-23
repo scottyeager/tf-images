da245f9f97b47469796ce56c8793b93b46d19f9c
Scott Yeager <yeagersm@gmail.com>
2024-02-23 09:59:46 -0800

    Generalized build for multiple dirs

--- fullvm/build.sh
+++ fullvm/build.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+path=$1
+name=$(basename $path)
+
 # Build the image from Dockerfile
-docker buildx build -t ubuntu:gridfullvm .
+pushd $path
+docker buildx build -t $name .
 
 # Extract image into tar archive
-container=$(docker create ubuntu:gridfullvm bash)
-docker export $container --output=ubuntu-fullvm.tar
+container=$(docker create $name bash)
+docker export $container --output=${name}.tar
 docker rm $container
 
 # Run touchup script, using Docker to avoid `sudo`
-container=$(docker create --entrypoint /touchup.sh ubuntu)
-docker cp ubuntu-fullvm.tar $container:/
-docker cp touchup.sh $container:/
+container=$(docker create -e ARCHIVE=${name}.tar --entrypoint /touchup.sh ubuntu)
+docker cp ${name}.tar $container:/
+docker cp ../touchup.sh $container:/
+
 docker start -a $container
-docker cp $container:/ubuntu-fullvm.tar.gz ./
+docker cp $container:/${name}.tar.gz ./
+
 docker rm $container
-rm ubuntu-fullvm.tar
+rm ${name}.tar
+popd
