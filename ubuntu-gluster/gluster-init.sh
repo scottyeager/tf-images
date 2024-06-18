105274338819b5496020ff852ef4bc3d2b161fd1
Scott Yeager <yeagersm@gmail.com>
2024-06-18 15:03:11 -0700

    Add timeout vars

--- ubuntu-gluster/gluster-init.sh
+++ ubuntu-gluster/gluster-init.sh
@@ -4,13 +4,13 @@ mkdir -p /data/gv0
 mkdir /mnt/gluster
 
 if [ $ME == gluster1 ]; then
-  sleep 2
+  sleep $TIMEOUT1
   gluster peer probe $PEER1
   gluster peer probe $PEER2
   gluster volume create gv0 replica 3 gluster1:/data/gv0 gluster2:/data/gv0 gluster3:/data/gv0 force
   gluster volume start gv0
 else
-  sleep 5
+  sleep $TIMEOUT2
 fi
 
 mount -t glusterfs ${ME}:/gv0 /mnt/gluster
