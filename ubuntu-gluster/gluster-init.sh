d3f2f3df728cb5bcdb9752b0e8ba3854c5d8b610
Scott Yeager <yeagersm@gmail.com>
2024-06-18 15:36:17 -0700

    Use host vars in volume create

--- ubuntu-gluster/gluster-init.sh
+++ ubuntu-gluster/gluster-init.sh
@@ -3,11 +3,11 @@
 mkdir -p /data/gv0
 mkdir /mnt/gluster
 
-if [ $LEADER ]; then
+if $LEADER; then
   sleep $TIMEOUT1
   gluster peer probe $PEER1
   gluster peer probe $PEER2
-  gluster volume create gv0 replica 3 gluster1:/data/gv0 gluster2:/data/gv0 gluster3:/data/gv0 force
+  gluster volume create gv0 replica 3 ${ME}:/data/gv0 ${PEER1}:/data/gv0 ${PEER2}:/data/gv0 force
   gluster volume start gv0
 else
   sleep $TIMEOUT2
