20c421e59010f342d5f7db08fc92cec5ac6a1974
Scott Yeager <yeagersm@gmail.com>
2024-06-18 17:56:19 -0700

    Fix etcd script

--- gluster-etcd/etcd.sh
+++ gluster-etcd/etcd.sh
@@ -5,5 +5,5 @@ etcd --name $ME --initial-advertise-peer-urls http://${MYIP}:2380 \
   --listen-peer-urls http://${MYIP}:2380 \
   --listen-client-urls http://${MYIP}:2379,http://127.0.0.1:2379 \
   --advertise-client-urls http://${MYIP}:2379 \
-  --initial-cluster-token $ETCD_CLUSTER_TOKEN
+  --initial-cluster-token $ETCD_CLUSTER_TOKEN \
   --initial-cluster-state new
